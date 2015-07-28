be49b490dae702d224f71491b08a983104a0f30b
Tanmay <tanmayss@gmail.com>
2015-07-28 11:11:14 -0400

    Test code

---
+++ DataStructures-In-C/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Tree/binaryTree.h"
+
+int main(void) {
+	TNodeRef tree = NULL;
+	tree = insert(tree, "i", "one");
+	tree = insert(tree, "j", "one");
+	tree = insert(tree, "a", "one");
+	tree = insert(tree, "b", "one");
+	tree = insert(tree, "j", "one");
+	tree = insert(tree, "x", "one");
+	tree = insert(tree, "z", "one");
+	
+	inOrderIterative(tree);
+
+	return EXIT_SUCCESS;
+}
